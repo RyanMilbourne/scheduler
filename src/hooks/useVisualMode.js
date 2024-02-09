1c3fbe68a8bd32280ab0fc2abaa630fe05811689
Ryan Milbourne <ryanmmilbourne@gmail.com>
2024-02-09 15:54:03 -0800

    add useVisualMode hook and incorporate functionality

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+const useVisualMode = (initial) => {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial])
+
+  const transition = (newMode, replace = false) => {
+    setMode(newMode)
+    setHistory(prev => (replace ? [...prev.slice(0, prev.length - 1), newMode] : [...prev, newMode]))
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1)])
+    }
+  }
+
+  return { mode: history[history.length - 1], transition, back };
+}
+
+export default useVisualMode;
